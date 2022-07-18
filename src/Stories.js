0aac58a66a081fe26d09d296ca425d8021d258a9
ThiagoCCR <thiago.ccr96@gmail.com>
2022-07-18 14:25:10 -0300

    fix stories

--- src/Stories.js
+++ src/Stories.js
@@ -2,25 +2,24 @@ import Story from "./Story";
 import ShowIonIcon from "./ShowIonIcon";
 
 let storiesArr =[
-      <Story Url="assets/img/9gag.svg" User="9gag" />,
-      <Story Url="assets/img/meowed.svg" User="meowed" />,
-      <Story Url="assets/img/barked.svg" User="barked" />,
-      <Story
-        Url="assets/img/nathanwpylestrangeplanet.svg"
-        User="nathanwpylestrangeplanet"
-      />,
-      <Story Url="assets/img/wawawicomics.svg" User="wawawicomics" />,
-      <Story Url="assets/img/respondeai.svg" User="respondeai" />,
-      <Story Url="assets/img/filomoderna.svg" User="filomoderna" />,
-      <Story Url="assets/img/memeriagourmet.svg" User="memeriagourmet" />,
-      <Story Url="assets/img/9gag.svg" User="9gag" />
+      {Url:"assets/img/9gag.svg", User:"9gag"},
+      {Url:"assets/img/meowed.svg", User:"meowed"},
+      {Url:"assets/img/barked.svg", User:"barked"},
+      {        Url:"assets/img/nathanwpylestrangeplanet.svg",
+        User:"nathanwpylestrangeplanet"
+    },
+      {Url:"assets/img/wawawicomics.svg", User:"wawawicomics"},
+      {Url:"assets/img/respondeai.svg", User:"respondeai"},
+      {Url:"assets/img/filomoderna.svg", User:"filomoderna"},
+      {Url:"assets/img/memeriagourmet.svg", User:"memeriagourmet"},
+      {Url:"assets/img/9gag.svg", User:"9gag"}
 
 ]
 
 export default function Stories() {
   return (
     <div class="stories">
-      {storiesArr.map((value)=> value)}
+      {storiesArr.map((storie)=> <Story {...storie} />)}
       <div class="setinha">
         <ShowIonIcon name="chevron-forward-circle" />
       </div>
